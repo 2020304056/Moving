c76e1fdbb46d42fc39d13272e26355dd85f86263
Lee Sang Hoon <sanghoon_2@naver.com>
2023-11-03 20:45:35 +0900

    FEAT : 어댑터 변경

---
+++ app/src/main/java/com/example/moving/Api.kt
@@ -0,0 +1,15 @@
+package com.example.moving
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key")
+        apiKey: String = "9897e125444076b7172d70bff4fe9c5d",
+        @Query("page")
+        page: Int
+    ): Call<GetMoviesResponse>
+}
--- app/src/main/java/com/example/moving/network/response/PeliculasResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moving.network.response
-
-import com.example.moving.models.PeliculaModel
-import com.google.gson.annotations.SerializedName
-
-data class PeliculasResponse(
-    @SerializedName("results")
-    var resultados: List<PeliculaModel>
-)
